9a6db1be2a3f592472cbe6cd0691508797204110
LSVMLUY <luciano.spinellivinas@mercadolibre.com>
2023-10-23 15:05:37 -0300

    modificacion prints sql_analysis

--- EDA/sql_analysis.py
+++ EDA/sql_analysis.py
@@ -2,17 +2,23 @@ import sqlite3
 import pandas as pd
 import os
 
+def print_resultados(title, query, df_query):
+    print(title)
+    print('"""')
+    print(query)
+    print('"""')
+    print(df_query)
+    print('')
+    
+    
 def sql_analysis():
     # Obtiene la ruta del directorio actual del script (directorios superiores)
     script_directory = os.path.dirname(os.path.abspath(__file__))
     parent_directory = os.path.dirname(script_directory)
     
     # Ruta relativa al directorio principal desde la subcarpeta ETL
-    db_path = os.path.join(parent_directory, 'mercadocredito.db')
-    print("db_path: "+ str(db_path))
-    print("current directory" + os.getcwd())
-    
-    
+    db_path = os.path.join(parent_directory, 'mercadocredito.db')    
+        
     # Conectar a la base de datos
     conn = sqlite3.connect(f'{db_path}')
     
@@ -33,13 +39,7 @@ def sql_analysis():
     df_query3 = pd.read_sql_query(query3, conn)
     
     # Preview de los resultados
-    print("Top 5 registros para Argentina y Uruguay \n")
-    print(df_query1.head(5))
-    
-    print("Top 5 registros cuya fecha de verificación es superior a Junio 2023 y cuya duración fue de más de 2 minutos\n")
-    print(df_query2.head(5))
-    
-    print("Top 5 registros para Argentina y Uruguay \n")
-    print(df_query3.head(5))
-    
+    print_resultados("Registros para Argentina y Uruguay \n",query1, df_query1)
+    print_resultados("Registros cuya fecha de verificación es superior a Junio 2023 y cuya duración fue de más de 2 minutos \n",query3, df_query2)
+    print_resultados("Registros de Brasil y dias Lunes \n",query3, df_query3)    
     conn.close()
